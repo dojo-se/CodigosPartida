ec7e42036a7acfd593aab2c79e3b167915f25389
Erick Mendonça <erickmendonca@yahoo.com.br>
2013-06-19 15:45:33 -0300

    Adiciona C#

---
+++ C#/CodigoPartidaCS/Dojo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodigoPartidaCS
+{
+    class Dojo
+    {
+        public int resultado = 1;
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    [TestClass]
+    public class DojoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Dojo dojo = new Dojo();
+            Assert.AreEqual(1, dojo.resultado);
+        }
+    }
+}
---
+++ C#/CodigoPartidaCS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informações Gerais sobre um assembly é controlado através do seguinte
+// conjunto de atributos. Altere estes valores de atributos para modificar as informações
+// associadas ao assembly.
+[assembly: AssemblyTitle("CodigoPartidaCS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodigoPartidaCS")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly não visíveis 
+// para componentes COM.  Caso precise acessar um tipo neste assembly a partir de 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir é para o ID da typelib se este projeto for exposto para COM
+[assembly: Guid("b0d20ea1-94b6-4272-84da-b6838595021d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Compilação
+//      Revisão
+//
+// É possível especificar todos os valores ou usar o padrão de números da Compilação e de Revisão 
+// usando o '*' como mostrado a seguir:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
