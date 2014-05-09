c76a3ca5f0729022f76daeeef7dc582a1c8d98a8
Erick Mendonca <erickmendonca@yahoo.com.br>
2014-05-09 15:33:54 -0300

    Adiciona segunda versão de C#

---
+++ C#/Solução2/VisualStudioTestPJ/Test01/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test01
+{
+
+    class Dojo
+    {
+        public int resultado = 1;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Dojo dojo = new Dojo();
+            Assert.AreEqual(1, dojo.resultado);
+        }
+
+        [TestMethod]
+        public void TestarErro()
+        {
+            Dojo dojo = new Dojo();
+            Assert.AreEqual(0, dojo.resultado);
+        }
+    }
+}
