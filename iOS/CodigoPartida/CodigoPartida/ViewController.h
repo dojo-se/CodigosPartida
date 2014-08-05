4c3024429ad9d39f04bfda860ef96edfb99c4d04
Hugo Doria <hugo@popcode.com.br>
2014-08-05 13:58:07 -0300

    adiciona o código de partida para iOS

---
+++ iOS/CodigoPartida/CodigoPartida/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodigoPartida
+//
+//  Created by Hugo Doria on 8/5/14.
+//  Copyright (c) 2014 Dojo-SE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS/CodigoPartida/CodigoPartida/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CodigoPartida
+//
+//  Created by Hugo Doria on 8/5/14.
+//  Copyright (c) 2014 Dojo-SE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
