1c3dcb3471f5d2ae23887520b7044c73d4e1b37e
Erick Mendonça <erickmendonca@yahoo.com.br>
2015-03-24 15:50:50 -0300

    Organiza arquivos de NodeJS

--- node-tdd/src/hello.js
+++
@@ -1,5 +0,0 @@
-function greet(name) {
-	return 'Hello, ' + name + '!';
-}
-
-module.exports.greet = greet;
--- node-tdd/test/hello.spec.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-var hello = require('../src/hello');
-
-describe('greeter', function(){
-	it('should say Hello to the World', function() {
-		assert.equal(hello.greet('World'),'Hello, World!');
-	});
-});
