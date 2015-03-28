af210941680440cd69381d50e914e014b970a3e8
Raphael Leão <raphaelpanta@gmail.com>
2015-03-28 10:23:41 -0300

    Atualizado classe de testes para português.

--- Scala/line_wrap_specs2_unit_style/src/test/scala/com/github/raphaelpanta/unit/WrapperTest.scala
+++ Scala/line_wrap_specs2_unit_style/src/test/scala/com/github/raphaelpanta/unit/WrapperTest.scala
@@ -8,34 +8,34 @@ import org.specs2.runner.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class WrapperTest extends Specification {
 
-  "The string 'I love you'" should {
-    "be wrapped at column 7" in {
+  "A frase 'I love you'" should {
+    "ser quebrada na coluna 7" in {
       "I love you" wrapColumnEach 7 must equalTo("I love\nyou")
     }
 
-    "be wrapped at column 10" in {
+    "ser quebrada na coluna 10" in {
       "I love you" wrapColumnEach 10 must equalTo("I love you");
     }
 
-    "be wrapped at column 1" in {
+    "ser quebrada na coluna 1" in {
       "I love you" wrapColumnEach 1 must equalTo("I\nlove\nyou");
     }
   }
 
-  "The string 'Um pequeno jabuti xereta viu dez cegonhas felizes.'" should {
-    "be wrapped at column 20" in {
+  "A frase 'Um pequeno jabuti xereta viu dez cegonhas felizes.'" should {
+    "ser quebrada na coluna 20" in {
       "Um pequeno jabuti xereta viu dez cegonhas felizes." wrapColumnEach 20 must equalTo("Um pequeno jabuti\nxereta viu dez\ncegonhas felizes.");
     }
   }
 
-  "The String 'Iloveyou'" should {
-    "be not be wrapped at all" in {
+  "A frase 'Iloveyou'" should {
+    "não deve ser quebrada" in {
       "Iloveyou" wrapColumnEach 0 must equalTo("Iloveyou")
     }
   }
 
-  "The string ''" should {
-    "return a empty string" in {
+  "A frase ''" should {
+    "retornar uma string vazia" in {
       "".wrapColumnEach(10) must equalTo("");
     }
   }
